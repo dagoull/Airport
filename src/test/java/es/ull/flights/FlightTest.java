abfdec461f4b6e20f70b1f6ccc40c03c0a0297c8
AlvaroGonzalezRodriguez <alu0101202556@ull.edu.es>
2021-12-12 13:05:07 +0000

    Creando prueba de Flight

--- src/test/java/es/ull/flights/FlightTest.java
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -1,4 +1,30 @@
 package es.ull.flights;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Pruebas para los vuelos")
+@Nested
 public class FlightTest {
+    private Flight flight;
+
+    @BeforeEach
+    void setUp() {
+        flight = new Flight("DV123", 100);
+    }
+
+    @Nested
+    @DisplayName("Existe un vuelo normal")
+    class NormalFlight {
+
+        @Test
+        @DisplayName("Se comprueba sus atributos")
+        public void testAtributosFlight() {
+            assertAll("Comprobando los atributos del vuelo",
+                    () -> assertEquals("DV123", flight.getFlightNumber()),
+                    () -> assertEquals(0, flight.getNumberOfPassengers())
+            );
+        }
+    }
+
 }
